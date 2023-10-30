4a47cbc37eb4482d7f36f8e9689a1442ded992c8
JooMin Choi <joominchoi23@gmail.com>
2023-10-30 14:15:31 +0900

    Create nav bar and console.log message to test its functionality

--- src/page-load.js
+++ src/page-load.js
@@ -9,8 +9,43 @@ function createHeader() {
   restaurantName.textContent = "flour and grape";
 
   header.appendChild(restaurantName);
+  header.appendChild(createNav());
 
   return header;
+}
+
+function createNav() {
+  const nav = document.createElement("nav");
+
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("nav-button");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    console.log("Home button has been clicked")
+  });
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("nav-button");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    console.log("Menu button has been clicked")
+  });
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("nav-button");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    console.log("Contact button has been clicked")
+  });
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+
+  return nav;
 }
 
 function createMainContainer() {
