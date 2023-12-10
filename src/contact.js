82082f5d17c3bc26ee60e8ea883b01d6db79c5ef
JooMin Choi <joominchoi23@gmail.com>
2023-12-10 17:55:29 +0000

    Update contact section with content

--- src/contact.js
+++ src/contact.js
@@ -6,7 +6,34 @@ function createContact() {
   description.classList.add('description');
   description.textContent = 'how to reach us'
 
+  const addressTitle = document.createElement("p");
+  addressTitle.classList.add('category')
+  addressTitle.textContent = 'address';
+
+  const address = document.createElement("p");
+  address.textContent = '111 Pasta Street, Olive Town, Italy, 111-1111';
+
+  const phoneTitle = document.createElement("p");
+  phoneTitle.classList.add('category')
+  phoneTitle.textContent = 'phone';
+
+  const phone = document.createElement("p");
+  phone.textContent = '111-1111-1111';
+
+  const emailTitle = document.createElement("p");
+  emailTitle.classList.add('category')
+  emailTitle.textContent = 'email';
+
+  const email = document.createElement("p");
+  email.textContent = 'flour@grape.com';
+
   contact.appendChild(description)
+  contact.appendChild(addressTitle)
+  contact.appendChild(address)
+  contact.appendChild(phoneTitle)
+  contact.appendChild(phone)
+  contact.appendChild(emailTitle)
+  contact.appendChild(email)
 
   return contact;
 }
